912cc5b7f4d76238cc850d7b46d602f0093901a0
Sasho <sn3ts@outlook.com>
2016-06-06 20:10:32 +0300

    SumArraysHomework

--- 3.ProgrammingFundamentals/2.DataTypesIMethodsIArraysILists/5.08-SumArrays/Program.cs
+++ 3.ProgrammingFundamentals/2.DataTypesIMethodsIArraysILists/5.08-SumArrays/Program.cs
@@ -9,20 +9,79 @@ namespace _5._08_SumArrays
         {
             int[] arrayOne = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             int[] arrayTwo = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            long arraySum = 0;
 
             if (arrayOne.Length == arrayTwo.Length)
             {
-                for (int i = 0; i < arrayOne.Length && i < arrayTwo.Length; i++)
+                SumArrays(arrayOne, arrayTwo);
+            }
+
+            else if (arrayOne.Length > arrayTwo.Length)
+            {
+                SumDifferentArrays(arrayOne, arrayTwo);
+            }
+
+            else if (arrayTwo.Length > arrayOne.Length)
+            {
+                SumDifferentArrays(arrayTwo, arrayOne);
+            }
+        }
+
+        public static void SumArrays(int[] arrayOne, int[] arrayTwo)
+        {
+            int[] resultArray = new int[arrayOne.Length];
+
+            for (int i = 0; i < arrayOne.Length && i < arrayTwo.Length; i++)
+            {
+                resultArray[i] = arrayOne[i] + arrayTwo[i];
+            }
+
+            Console.Write(String.Join(" ", resultArray));
+        }
+
+        public static void SumDifferentArrays(int[] arrayOne, int[] arrayTwo)
+        {
+            int[] resultArray = new int[] { };
+
+            resultArray = new int[arrayOne.Length];
+            int startRepeatIndex = 0;
+            int endRepeatIndex = arrayTwo.Length;
+
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                if (i < arrayTwo.Length)
                 {
-                    arraySum = arrayOne[i] + arrayTwo[i];
-                    Console.Write(arraySum + " ");
+                    resultArray[i] = arrayTwo[i];
+                }
+                else
+                {
+                    if (startRepeatIndex < endRepeatIndex)
+                    {
+                        resultArray[i] = arrayTwo[startRepeatIndex];
+                        startRepeatIndex++;
+                    }
+
+                    if (startRepeatIndex == endRepeatIndex)
+                    {
+                        startRepeatIndex = 0;
+                    }
                 }
             }
-            if (arrayOne.Length > arrayTwo.Length)
-            {
-                
-            }
+
+            SumArrays(arrayOne, resultArray);
         }
     }
 }
+
+
+
+//var arr1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//var arr2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//var n = Math.Max(arr1.Length, arr2.Length);
+//var sumArr = new int[n];
+
+//            for (int i = 0; i<n; i++)
+//            {
+//                sumArr[i] = arr1[i % arr1.Length] + arr2[i % arr2.Length];
+//            }
+
+//            Console.WriteLine(string.Join(" ", sumArr));
